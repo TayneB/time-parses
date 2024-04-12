cc1fdb4598be3731b7eaf0c48b29d41b6248e992
Tayne Bilyard <taynebilyard@gmail.com>
2024-04-12 14:13:16 +1200

    mappable parseList component created

--- client/components/parseList.tsx
+++ client/components/parseList.tsx
@@ -1,3 +1,24 @@
-function ParseList() {
-  return <></>
+function ParseList(
+  name: string,
+  spec: string,
+  duration: number,
+  code: string,
+  fightID: number
+) {
+  const modDuration = duration / 1000
+
+  return (
+    <>
+      <p>
+        {name} - {spec} - {modDuration} -{' '}
+        <a
+          href={`https://www.warcraftlogs.com/reports/${code}#fight=${fightID}`}
+        >
+          Report
+        </a>
+      </p>
+    </>
+  )
 }
+
+export default ParseList
