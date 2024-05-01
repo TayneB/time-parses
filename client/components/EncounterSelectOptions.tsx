680f3a37616619a514c861ec4767d15d7e9224fb
Tayne Bilyard <taynebilyard@gmail.com>
2024-05-01 19:08:57 +1200

    removed vestigial dependency declaration

--- client/components/EncounterSelectOptions.tsx
+++ client/components/EncounterSelectOptions.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useEncounterIds } from '../hooks/useParses'
 
 const EncounterSelectOptions = ({
